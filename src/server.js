9f4b87beb5b27f80434ad58cd96b615439d41058
Diego Pereira <diegogomespereira@gmail.com>
2020-07-10 22:14:17 -0400

    Cleanup some unused code

--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
-import dotenv from 'dotenv'
+import { config } from 'dotenv'
 import sirv from 'sirv'
 import polka from 'polka'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
 
-const config = dotenv.config()
+config()
+
 const { PORT, NODE_ENV, GITHUB_TOKEN } = process.env
 const dev = NODE_ENV === 'development'
 
 polka()
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware({
-			session: (req, res) => ({ GITHUB_TOKEN })
-		})
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	})
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware({
+      session: (req, res) => ({ GITHUB_TOKEN })
+    })
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err)
+  })
